1dac91e116b83e39a472fccace0c0610c6bc702f
Ortegafa <ortegafa@uninorte.edu.co>
2023-05-12 23:54:36 -0500

    todo listo falta el script
    falta el script

--- Lab3_estructura/Assets/Scripts/Ahorcado.cs
+++ Lab3_estructura/Assets/Scripts/Ahorcado.cs
@@ -1,15 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ahorcado : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //si Alguien quiere trabajar en esta monda, es libre de borrar toda esta verga    
+    private string dato;
+   [SerializeField] private UnityEngine.UI.Text componenteTexto = null;
+    public InputField palabraInput;
+    public Text resultadoText;
+
+    public void Generar()
+    {
+        string palabra = palabraInput.text;
+        string resultado = GenerarLineas(palabra);
+        resultadoText.text = resultado;
+    }
+
+    private string GenerarLineas(string palabra)
+    {
+        string lineas = "";
+
+        foreach (char caracter in palabra)
+        {
+        lineas += "_";
+        }
+
+        return lineas;
+    }
     void Start()
     {
         
     }
+    
+    public void Ingresodato(string s){
+        dato=s;
+        Debug.Log("funciona");
 
+    }
+    public void cantidad_caracteres(){
+    int cantidad= dato.Length;
+    string palabra= "la palabra tiene una cantidad de caracteres" + dato.Length;
+    componenteTexto.text=palabra;
+    Debug.Log("funciona pero xd");
+
+}
     // Update is called once per frame
     void Update()
     {
--- Lab3_estructura/Assets/Scripts/GameManager.cs
+++ Lab3_estructura/Assets/Scripts/GameManager.cs
@@ -2,8 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public enum GameState{
+    menu,
+    inGame,
+    gameOver
+}
+public class GameManager : MonoBehaviour
 {
+    public  GameState currentGameState = GameState.menu;
+    public static GameManager sharedInstance;
+
+    void Awake(){
+        if(sharedInstance==null){
+            sharedInstance = this;
+        }
+
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +29,33 @@ public class NewBehaviourScript : MonoBehaviour
     {
         
     }
+    //inicio del juego
+    public void StartGame(){
+            SetGameState(GameState.inGame); 
+    }
+    //fin del juego
+    public void GameOver(){
+            SetGameState(GameState.gameOver);
+    }
+    //devolver al menu 
+    public void BackToMenu(){
+            SetGameState(GameState.menu);
+    }
+
+    private void SetGameState(GameState newGameSate){
+        if(newGameSate == GameState.menu){
+             //Todo: logica del menu
+             MenuManager.sharedInstance.ShowMainMenu();
+
+        }else if(newGameSate == GameState.inGame){
+            //preparar la escena 
+            MenuManager.sharedInstance.HideMainMenu();
+        
+        }else if(newGameSate == GameState.gameOver){
+            //Que hacer en caso del game Over
+            MenuManager.sharedInstance.ShowMainMenu();  
+       }
+        this.currentGameState=newGameSate;
+    }
+
 }
