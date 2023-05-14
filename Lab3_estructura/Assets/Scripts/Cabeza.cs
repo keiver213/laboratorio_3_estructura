2b0d490479fd2bd03f852aa6a17097845efe5014
keiver213 <127407446+keiver213@users.noreply.github.com>
2023-05-14 16:27:02 -0500

    animacion cabeza intro, escena intro, sistema completo de escritura
    todo terminado falta logica

--- Lab3_estructura/Assets/Scripts/Cabeza.cs
+++ Lab3_estructura/Assets/Scripts/Cabeza.cs
@@ -6,19 +6,33 @@ public class Cabeza : MonoBehaviour
 {
     public int contador;
     public pala palabras;
+    [SerializeField]
     public Animator animacion;
-    
+    GameObject cabeza;
+
     void Start()
     {
-        animacion = GetComponent<Animator>();
+        cabeza = GameObject.FindWithTag("Player");
+        animacion = cabeza.GetComponent<Animator>();
     }
 
     void Update()
     {
-        if (palabras.errores == 1)
+        if (palabras.errores == 2)
         {
-            Debug.Log("puta vida");
             animacion.SetBool("error", true);
+        }
+        
+        if (palabras.errores == 4)
+        {
+            animacion.SetBool("error", false);
+            animacion.SetBool("error2", true);
+        }
+        
+        if (palabras.errores == 6)
+        {
+            animacion.SetBool("error2", false);
+            animacion.SetBool("error3", true);
         }
     }
 }
