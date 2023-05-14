c71254756e0944f421c6bf62ab9e481570749aea
keiver213 <127407446+keiver213@users.noreply.github.com>
2023-05-14 00:04:37 -0500

    validacion numeros

--- Lab3_estructura/Assets/Scripts/pala.cs
+++ Lab3_estructura/Assets/Scripts/pala.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Text.RegularExpressions;
 
 public class pala : MonoBehaviour
 {
     public InputField textito;
     public Text textoerror;
+    string pattern = @"^[^\d]+$";
 
     void Start()
     {
@@ -15,7 +17,7 @@ public class pala : MonoBehaviour
 
     public void get_text()
     {
-        if (textito.text.Length == 1)
+        if (textito.text.Length == 1 && Regex.IsMatch(textito.text, pattern))
         {
             string texto = textito.text;
             Debug.Log("La palabra ingresada fue: " + texto);
